b68b7da29e331f67d6cacac48477dcc26f8266e0
Kukovich <69118658+Kukovich@users.noreply.github.com>
2021-03-28 18:46:10 +0300

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,46 @@
+import random
+
+def line():
+    print("------------------------------------------------------------------------------------------------")
+
+lt = list()
+total = 0
+act = 0
+a = input("Введите длину списка: ")
+
+if a.isdigit():
+    if int(a) != 0:
+        
+        line()
+        print("Первый способ:")
+
+        while act != int(a):
+            num = random.choice(range(0, 20001))
+            lt.append(num)
+            total = total + num
+            act = act + 1
+
+        print("Сумма значений списка =", total, """
+        Среднее арифметическое =""", int(total) / int(a))
+        line()
+        print("Второй способ:")
+
+        act = 0
+        total = 0
+        lt.clear()
+
+        for two in range(int(a)):
+            num = random.choice(range(0, 20001))
+            lt.append(num)
+            total = total + num
+            act = act + 1
+        
+        print("Сумма значений списка =", total, """
+        Среднее арифметическое =""", int(total) / int(a))
+        line()
+
+    else:
+        print("Длина списка должна быть больше нуля")
+
+else:
+    print("Введите правильное значение")
